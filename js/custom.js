e5d0b052a0cf29475f658a3dee081095d6f92ba1
Rahul-Dhara <151688372+Rahul-Dhara@users.noreply.github.com>
2023-11-22 04:33:59 +0530

    farhakhan

---
+++ js/custom.js
@@ -0,0 +1,86 @@
+$(document).ready(function(){
+
+    
+
+  $('.holderslider').owlCarousel({
+    loop:true,
+    margin:5,
+     nav:true,
+     autoplay:true,
+     autoplayTimeout:3000,
+     slideSpeed: 5000,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:1,
+        },
+        1000:{
+            items:1,
+        }
+    }
+})
+
+ 
+ 
+
+
+
+})
+
+
+//in-view-js
+// var $animation_elements = $('.animation-element');
+// var $window = $(window);
+
+// function check_if_in_view() {
+//   var window_height = $window.height();
+//   var window_top_position = $window.scrollTop();
+//   var window_bottom_position = (window_top_position + window_height);
+ 
+//   $.each($animation_elements, function() {
+//     var $element = $(this);
+//     var element_height = $element.outerHeight();
+//     var element_top_position = $element.offset().top;
+//     var element_bottom_position = (element_top_position + element_height);
+ 
+//     if ((element_bottom_position >= window_top_position) &&
+//         (element_top_position <= window_bottom_position)) {
+//       $element.addClass('in-view');
+//     } else {
+//       $element.removeClass('in-view');
+//     }
+//   });
+// }
+
+// $window.on('scroll resize', check_if_in_view);
+// $window.trigger('scroll');
+
+// $(window).scroll(function(){
+//     if($(this).scrollTop() > 600){
+//         $('.scrolltop').css("opacity","1");
+//     }else{
+//         $('.scrolltop').css("opacity","0");
+//     }
+// });
+
+// var count_particles, stats, update;
+// stats = new Stats;
+// stats.setMode(0);
+// stats.domElement.style.position = 'absolute';
+// stats.domElement.style.left = '0px';
+// stats.domElement.style.top = '0px';
+// document.body.appendChild(stats.domElement);
+// count_particles = document.querySelector('.js-count-particles');
+// update = function() {
+//   stats.begin();
+//   stats.end();
+//   if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+//     count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+//   }
+//   requestAnimationFrame(update);
+// };
+// requestAnimationFrame(update);
+
